75d79508150207812842248331f287026ae7f70e
Riley <rileycarr2005@gmail.com>
2025-09-17 12:35:19 -0500

    updated code to work with buttons

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
+const container = document.querySelector('.container')
+const scissorsBtn = document.querySelector('.scissors');
+const paperBtn = document.querySelector('.paper');
+const rockBtn = document.querySelector('.rock');
 
-    if (humanScore > computerScore) {
-        console.log('You win against the computer!!');
-    } else if (humanScore < computerScore) {
-        console.log('You lose to the computer!! :( LMAOOOOO');
+scissorsBtn.addEventListener('click', () => playRound('scissors', getComputerChoice()));
+paperBtn.addEventListener('click', () => playRound('paper', getComputerChoice()));
+rockBtn.addEventListener('click', () => playRound('rock', getComputerChoice()));
+
+const winnerDiv = document.createElement('div');
+
+function playRound(humanChoice, computerChoice) {
+    let winnerText = '';
+
+    if (humanChoice == computerChoice) {
+        winnerText = 'Tie! Nobody wins!';
     } else {
-        console.log('Somehow u tied a best of 5 for this, so good on you I guess. I mean u didn\'t lose.');
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == computerChoice) {
-            console.log('Tie! Nobody wins!');
-        } else {
-            if (humanChoice == 'rock') {
-                if (computerChoice == 'paper') {
-                    console.log('You lose!');
-                    computerScore++;
-                } else {
-                    console.log('You win');
-                    humanScore++;
-                }
-            } else if (humanChoice == 'paper') {
-                if (computerChoice == 'rock') {
-                    console.log('You win!');
-                    humanScore++;
-                } else {
-                    console.log('You Lose!');
-                    computerScore++;
-                }
-            } else if (humanChoice == 'scissors') {
-                if (computerChoice == 'paper') {
-                    console.log('You win!');
-                    humanScore++;
-                } else {
-                    console.log('You lose!');
-                    computerScore++;
-                }
+        if (humanChoice == 'rock') {
+            if (computerChoice == 'paper') {
+                winnerText = 'You lose!';
+                computerScore++;
             } else {
-                console.log('Not a valid option! You lose!');
+                winnerText = 'You win';
+                humanScore++;
+            }
+        } else if (humanChoice == 'paper') {
+            if (computerChoice == 'rock') {
+                winnerText = 'You win!';
+                humanScore++;
+            } else {
+                winnerText = 'You Lose!';
                 computerScore++;
             }
-        }
-    }
-
-    function getComputerChoice() {
-        const randNum = Math.floor((Math.random() * 3) + 1);
-        if (randNum == 1) {
-            return 'paper';
-        } else if (randNum == 2) {
-            return 'rock';
+        } else if (humanChoice == 'scissors') {
+            if (computerChoice == 'paper') {
+                winnerText = 'You win!';
+                humanScore++;
+            } else {
+                winnerText = 'You lose!';
+                computerScore++;
+            }
         } else {
-            return 'scissors';
+            winnerText = 'Not a valid option! You lose!';
+            computerScore++;
         }
     }
+    const winner = checkWinner()
+    if (winner !== null) {
+        winnerText += winner;
+    }
+    
+    winnerDiv.textContent = winnerText;
+    winnerDiv.style.color = 'blue';
+    container.appendChild(winnerDiv);
+}
 
-    function getHumanChoice() {
-        const choice = prompt("What is your choice?");
-        return choice.toLowerCase();
+function getComputerChoice() {
+    const randNum = Math.floor((Math.random() * 3) + 1);
+    if (randNum == 1) {
+        return 'paper';
+    } else if (randNum == 2) {
+        return 'rock';
+    } else {
+        return 'scissors';
+    }
+}
+
+function checkWinner() {
+    if (humanScore === 5) {
+        return 'You win against the computer!!';
+    } else if (computerScore === 5) {
+        return 'You lose to the computer!! :( LMAOOOOO';
+    } else {
+        return null;
     }
 }
